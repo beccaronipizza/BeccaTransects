49ad5f8a0787a2ac35c63e575726b029d68367e7
beccaronipizza <145154447+beccaronipizza@users.noreply.github.com>
2023-09-15 14:47:28 -0700

    Update domComm.R
    Revised to include Total veg as its own column

--- code/domComm.R
+++ code/domComm.R
@@ -1,13 +1,19 @@
 library(readxl)
 library(tidyverse)
 
-domComm <- read_xlsx("data/VegData.xlsx", sheet = "Data") %>%  
+  vegdata <- read_xlsx("data/VegData.xlsx", sheet = "Data")
+  
+  
+DominantCommunity <- vegdata %>% 
   select(Month:Percent_Bare_Ground) %>% 
-  pivot_longer(cols = Percent_Spartina:Percent_Bare_Ground,
+  mutate(Date = paste0(Month, "_", Year)) %>% 
+  pivot_longer(cols = c("Percent_Spartina", "Percent_Pickleweed", "Percent_Upland", "Percent_Bare_Ground"),
                names_to = "Community") %>%
   nest(data = c("Community", "value")) %>%
   mutate(domComm = map(.x = data, ~slice_max(.x, order_by = value, n = 1)),
          .keep = "unused") %>%
-  unnest(domComm)
+  unnest(domComm) %>% 
+  mutate(Community = str_remove(Community, "Percent_"))
 
-domComm
+#nest-mutate-map: use when you want to summarize on groups of rows *watch video*
+#remember to assign a variable name like domComm at the beginning "name <- ..."
